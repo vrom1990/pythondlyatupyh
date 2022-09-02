2e70eeabbd39366937dd91147d2f93d93169e970
Славелий Роза <utkorose@yandex.ru>
2022-09-02 21:48:02 +0700

    Папку переименовал

---
+++ 010elif/07.py
@@ -0,0 +1,33 @@
+numeral = input('Введите цифру: ')
+if numeral == '0':
+    numeral = 'ноль'
+    print(numeral)
+elif numeral == '1':
+    numeral = 'один'
+    print(numeral)
+elif numeral == '2':
+    numeral = 'два'
+    print(numeral)
+elif numeral == '3':
+    numeral = "три"
+    print(numeral)
+elif numeral == '4':
+    numeral = "четыре"
+    print(numeral)
+elif numeral == '5':
+    numeral = "пять"
+    print(numeral)
+elif numeral == '6':
+    numeral = 'шесть'
+    print(numeral)
+elif numeral == '7':
+    numeral = 'семь'
+    print(numeral)
+elif numeral == '8':
+    numeral = 'восемь'
+    print(numeral)
+elif numeral == '9':
+    numeral = 'девять'
+    print(numeral)
+else:
+    print('Это не цифра')
