d37805827aa7860c15d48df7bcd8c2dab88704a5
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2023-02-21 23:02:24 +0400

    Update 3.py

--- 011.6answers/3.py
+++ 011.6answers/3.py
@@ -1,7 +1,7 @@
-from random import random
+from random import randint
 PAROL = "123456"
 parol = input("Введите пароль: ")
 if parol == PAROL:
-    print("ЙЕССС")
+    code = randint(1000,9999)
 else:
     print("Неверный пароль")
