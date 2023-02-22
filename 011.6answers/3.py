847658e9fd42eedf86d43939e85c30010535df31
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2023-02-22 23:16:19 +0400

    Update 3.py

--- 011.6answers/3.py
+++ 011.6answers/3.py
@@ -2,6 +2,10 @@ from random import randint
 PAROL = "123456"
 parol = input("Введите пароль: ")
 if parol == PAROL:
-    code = randint(1000,9999)
+    code = str(randint(1000,9999))
+    print("Ваш код: " + code)
+    my_code = input("Введите код из СМС")
+    if my_code == code:
+        pass
 else:
     print("Неверный пароль")
