575218069d85b0af279b4b23894f068075e875a6
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2023-02-27 18:46:22 +0400

    Update 3.py

--- 011.6answers/3.py
+++ 011.6answers/3.py
@@ -4,12 +4,14 @@ parol = input("Введите пароль: ")
 if parol == PAROL:
     code = str(randint(1000,9999))
     print("Ваш код: " + code)
-    my_code = input("Введите код из СМС")
+    my_code = input("Введите код из СМС: ")
     if my_code == code:
         moj_palets = input("Успешное сканирование?: ")
         if moj_palets == "да":
             print("Успешный вход в систему!")
         else:
             print("Неверный отпечаток пальца!")
+    else:
+        print("Код неверный")
 else:
     print("Неверный пароль")
