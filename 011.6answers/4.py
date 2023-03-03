de1e38bf1916297fa35fb714e383a04ebc263ae8
Вячеслав Романьков <46442686+vrom1990@users.noreply.github.com>
2023-03-03 16:36:19 +0400

    Update 4.py

--- 011.6answers/4.py
+++ 011.6answers/4.py
@@ -9,14 +9,19 @@ m8 = 9
 m9 = 10
 m10 = 10
 
-if m1+m2+m3 > m4+m5+m6:
-    if m4 > m5:
-        pass
-    elif m4 < m5:
+if m1+m2+m3 < m4+m5+m6:
+    if m1 < m2:
+        print("Первая фальшивая")
+    elif m1 > m2:
+        print("Вторая фальшивая")
+    else:
+        print("Третья фальшивая")
+elif m1+m2+m3 > m4+m5+m6:
+    if m4 < m5:
+        print("Четвёртая фальшивая")
+    elif m4 > m5:
         pass
     else:
         pass
-elif m1+m2+m3 < m4+m5+m6:
-    pass
 else:
     pass
